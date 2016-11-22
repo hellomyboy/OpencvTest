e513a2dbb74b31652a972dda0463e95ef0fabde9
hellomyboy <ht0411@qq.com>
2016-11-22 23:09:03 +0800

    image process test
    Signed-off-by: hellomyboy <ht0411@qq.com>

--- test01/test01/main.cpp
+++ test01/test01/main.cpp
@@ -1,10 +1,38 @@
-#include <cv.h>
+﻿#include <cv.h>
 #include <highgui.h>
 using namespace std;
+
+void guitest(char* filename)
+{
+	cvNamedWindow("win1", CV_WINDOW_AUTOSIZE);
+	cvMoveWindow("win1", 100, 100);
+
+	IplImage* img = 0;
+	img = cvLoadImage(filename);
+	if (!img) {
+		printf("cannot load image file: %s\n", filename);
+	}
+
+	cvShowImage("win1", img);
+	cvWaitKey(0);
+	cvDestroyWindow("win1");
+}
+
+void videotest()
+{
+	CvCapture* capture = cvCaptureFromCAM(0);
+}
+
 int main()
 {
+	guitest("C:\\Users\\think\\Desktop\\terrain.jpg");
+	return 0;
+}
+
+int test01()
+{
 	IplImage * test;
-	test = cvLoadImage("C:\\Users\\huangtao3\\Desktop\\Grass.png");//ͼƬ·��
+	test = cvLoadImage("‪C:\\Users\\think\\Desktop\\leaf01.png");//图片路径
 	cvNamedWindow("test_demo", 1);
 	cvShowImage("test_demo", test);
 	cvWaitKey(0);
